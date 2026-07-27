59597fa2eb281d71a2eeef87ecd739a620ffd341
lechk82 <lechk82@googlemail.com>
2026-07-27 22:47:19 +0200

    feat: add support for firmware release messages, rename spiffs to wallpaper

--- version.js
+++ version.js
@@ -1,2 +1,10 @@
 window.SMARTROSARY_VERSION = "v1.48";
 window.SMARTROSARY_LANG_VERSION = "v1.31";
+window.SMARTROSARY_RELEASE_MESSAGE = {
+  "en": "",
+  "de": "",
+  "pl": "",
+  "fr": "",
+  "es": "",
+  "pt": ""
+};
