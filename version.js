c0a32cbde56edf28b6ae876a60ba55bf02ea19b8
lechk82 <lechk82@googlemail.com>
2026-07-31 18:50:39 +0200

    Publish lang v1.37

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 window.SMARTROSARY_VERSION = "v1.55";
-window.SMARTROSARY_LANG_VERSION = "v1.36";
+window.SMARTROSARY_LANG_VERSION = "v1.37";
 window.SMARTROSARY_BREAKING_CHANGES = {
   "versions": { "before": "v1.49" },
   "messages": {
