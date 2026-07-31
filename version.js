aaa53554c9c7c11900e8e19e4757b67db60178d9
lechk82 <lechk82@googlemail.com>
2026-07-31 18:28:39 +0200

    fix: use proper LVGL glyph for firmware upgrade arrow in l057 (lang v1.36)

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 window.SMARTROSARY_VERSION = "v1.54";
-window.SMARTROSARY_LANG_VERSION = "v1.35";
+window.SMARTROSARY_LANG_VERSION = "v1.36";
 window.SMARTROSARY_BREAKING_CHANGES = {
   "versions": { "before": "v1.49" },
   "messages": {
