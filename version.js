f7ba4209a322786a2b33d2b505edb391edfbbca2
lechk82 <lechk82@googlemail.com>
2026-08-30 19:32:16 +0200

    Release language binaries v1.45

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 window.SMARTROSARY_VERSION = "v1.64";
-window.SMARTROSARY_LANG_VERSION = "v1.44";
+window.SMARTROSARY_LANG_VERSION = "v1.45";
 window.SMARTROSARY_BREAKING_CHANGES = {
   "versions": { "before": "v1.49" },
   "messages": {
